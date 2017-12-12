0b25718bb4b64ef9c5b24b565a54a5372e00caf7
Peniel Tambama <tambama.peniel@outlook.com>
2017-12-12 12:17:52 +0200

    Now Playing

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-frontend-nanodegree-arcade-game
-===============================
+# frontend-nanodegree-arcade-game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## How to run arcade game
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+* Open index.html in a browser
+
+## How to Play
+
+1. Use left arrow to move player to the left
+2. Use right arrow to move player to the right
+3. Use up arrow to move player up
+4. Use down arrow to move player down
+
+## Game rules
+
+* You must reach the water to win
+* When you collide with enemy the game is finished and your score is set to zero
--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
+// Sets an initial player score of 0.
+var score = 0;
+document.getElementById('score').innerHTML = score;
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,17 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x < 505) {
+        this.x += (150 * dt);
+    }
+    else {this.x = -90;}
+
+	// On collision of enemy and player
+    if(this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 60 && this.y + 40 > player.y) {
+		score = 0;
+		document.getElementById('score').innerHTML = score;
+		player.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +41,60 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 320;
+};
+
+// Update player position
+Player.prototype.update = function() {
+    
+   // player gets to the water
+   if (player.y < 20) {
+   score++;
+   document.getElementById('score').innerHTML = score;
+   this.reset();
+}
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+    if(direction == 'left' && this.x > 0) {
+        this.x -= 50;
+    }
+    if(direction == 'right' && this.x < 400) {
+        this.x += 50;
+    }
+    if(direction == 'up' && this.y > 3) {
+        this.y -= 50;
+    }
+    if(direction == 'down' && this.y < 400) {
+        this.y += 50;
+    }
+};
+
+// Reset player
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 320;
+};
 
 
 // Now instantiate your objects.
+var enemyOne = new Enemy(-90, 60);
+var enemyTwo = new Enemy(-390, 140);
+var enemyThree = new Enemy(-290, 230);
+var enemyFour = new Enemy(-190, 140);
+var enemyFive = new Enemy(-490, 60);
+var enemySix = new Enemy(-890, 230);
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [enemyOne, enemyTwo, enemyThree, enemyFour, enemyFive, enemySix];
 // Place the player object in a variable called player
-
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
